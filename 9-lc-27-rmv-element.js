3b1aeea705f9ce6b5f4651693e3f2d136bf51db6
Arman-Kabir <armankabir8@gmail.com>
2023-03-03 10:16:59 +0600

    lc27 done

---
+++ 9-lc-27-rmv-element.js
@@ -0,0 +1,20 @@
+
+
+// 27. Remove Element
+var removeElement = function (nums, val) {
+    let l = 0;
+    console.log(nums, val);
+
+    for (let i = 0; i < nums.length; i++) {
+        console.log(i);
+        if (nums[i] != val) {
+            nums[l] = nums[i];
+            l++;
+        } else {
+            continue;
+        }
+    }
+    return l;
+};
+
+console.log(removeElement([0, 1, 2, 2, 3, 0, 4, 2], 2));
